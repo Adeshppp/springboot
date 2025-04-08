2ca36ec663cca804e2b077278b1663d91d0f9bcb
Adesh <adeshpadwal21@gmail.com>
2025-04-08 17:52:28 -0400

    postgresql database connectivity project

---
+++ PostgresqlDatabase/src/main/java/com/springboot/PostgresqlDatabase/PostgresqlDatabaseApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.PostgresqlDatabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostgresqlDatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostgresqlDatabaseApplication.class, args);
+	}
+
+}
---
+++ PostgresqlDatabase/src/test/java/com/springboot/PostgresqlDatabase/PostgresqlDatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.PostgresqlDatabase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostgresqlDatabaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
