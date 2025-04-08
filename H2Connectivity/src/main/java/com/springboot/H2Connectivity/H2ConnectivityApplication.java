a4b9276b4aa7b3bb77c44ffe40adf82c7567884a
Adesh <adeshpadwal21@gmail.com>
2025-04-08 17:34:15 -0400

    h2 databse connectivity project

---
+++ H2Connectivity/src/main/java/com/springboot/H2Connectivity/H2ConnectivityApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.H2Connectivity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class H2ConnectivityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(H2ConnectivityApplication.class, args);
+	}
+
+}
---
+++ H2Connectivity/src/test/java/com/springboot/H2Connectivity/H2ConnectivityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.H2Connectivity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class H2ConnectivityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
