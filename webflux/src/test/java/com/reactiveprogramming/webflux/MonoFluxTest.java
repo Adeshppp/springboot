ae92a7339add74d2e5ac763662cd7aa8994153e4
Adesh <adeshpadwal21@gmail.com>
2025-04-10 08:05:47 -0400

    test flux

--- webflux/src/test/java/com/reactiveprogramming/webflux/MonoFluxTest.java
+++ webflux/src/test/java/com/reactiveprogramming/webflux/MonoFluxTest.java
@@ -1,6 +1,7 @@
 package com.reactiveprogramming.webflux;
 
 import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 public class MonoFluxTest {
@@ -13,4 +14,13 @@ public class MonoFluxTest {
                 .log();
         monoString.subscribe(System.out::println,(e)-> System.out.println(e.getMessage()));
     }
+
+
+
+    @Test
+    public void testFlux(){
+        Flux<String> fluxString = Flux.just("Spring","Spring boot","hibernate","reactive programming").log();
+        fluxString.subscribe(System.out::println);
+    }
+
 }
